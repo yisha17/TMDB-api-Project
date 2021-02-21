ad4783210636b3e9197e07933a79597607d88b78
Maria GebreMedhin <it.maria.geberemedhin@gmail.com>
2021-02-22 09:43:33 +1200

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+const MOVIE_DB_API = 'd8bf019d0cca372bd804735f172f67e8';
+const MOVIE_DB_ENDPOINT = 'https://api.themoviedb.org';
+const MOVIE_DB_IMAGE_ENDPOINT = 'https://image.tmdb.org/t/p/w500';
+const DEFAULT_POST_IMAGE = 'https://via.placeholder.com/150';
+
+function requestMovies(url, onComplete, onError) {
+    fetch(url)
+        .then((res) => res.json())
+        .then(onComplete)
+        .catch(onError);
+}
